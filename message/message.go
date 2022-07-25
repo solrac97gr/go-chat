e77b17c06315b5574f3185b3fd1f52cf06aa18f5
carlos97gr <carlosgperu97@gmail.com>
2022-07-25 23:13:22 +0300

    fixing server messages type

--- message/message.go
+++ message/message.go
@@ -12,7 +12,7 @@ func NewMessageService() *Service {
 	return &Service{}
 }
 
-func (s *Service) MessageWrite(conn net.Conn, messages chan<- string) {
+func (s *Service) MessageWrite(conn net.Conn, messages <-chan string) {
 	for msg := range messages {
 		fmt.Fprintln(conn, msg)
 	}
